f40c26c74a875453d319cefc0fbde5555ce6d83b
erciccione <erciccione@protonmail.com>
2020-12-15 17:44:32 +0000

    Ask for user input as soon as possible

--- monero-static.sh
+++ monero-static.sh
@@ -17,6 +17,28 @@ get_tag() {
   tag=$(curl -s -o- https://api.github.com/repos/monero-project/monero-gui/releases/latest | jq -r '.tag_name') || exit
 }
 
+get_build() {
+
+  printf "\nWhat version do you want to build?\n"
+  printf "\nmaster is bleeding edge and can contain bugs.\nBuild the latest release if you are unsure.\n\n"
+  printf "1) Build master"
+  printf "\n2) Build latest release: $tag\n"
+  leaveready="\nWhat do you want to do? Choose 1 or 2.\nPress ctrl + c or type 'exit' if you want to leave:\n"
+  printf "$leaveready"
+
+  while true; do
+    read version
+
+    if [[ $version -eq 1 ]] || [[ $version -eq 2 ]]; then
+      break
+    elif [[ $version -eq "exit" ]]; then
+      exit
+    else
+      printf "$leaveready"
+    fi
+  done
+}
+
 build() {
 
   dexec() {
@@ -26,37 +48,19 @@ build() {
   copy() {
   printf "\n-> Importing binaries..."
   docker cp $1 . && printf "\n-> Binaries copied to 'bin' folder in " && pwd || printf "\n-> Something went wrong, binaries not copied" && exit
-  
   }
 
-  printf "\n-> Fetching code...\n"
-
-  printf "\nWhat version do you want to build?\n"
-  printf "\nmaster is bleeding edge and can contain bugs. Build the latest release if you are unsure.\n\n"
-  printf "1) Build master"
-  printf "\n2) Build latest release: $tag\n"
-  printf "\nWhat do you want to do? (Choose 1 or 2. Press ctrl + c or type 'exit' if you want to leave)\n"
-
-  while true; do
-    read version
-
-    if [[ $version -eq 1 ]]; then
-      printf "\n-> Building Master...\n"
-      dexec "git pull && git submodule update --init --force && make release-static"
-      copy monero-static-container:/home/monero/build/Linux/master/release/bin
-      break
-    elif [[ $version -eq 2 ]]; then
-      printf "\n-> Building release $tag\n"
-      dexec "git pull --all && git checkout ${tag} &> /dev/null && git submodule update --init --force && make release-static -j3"
-      copy monero-static-container:/home/monero/build/Linux/_HEAD_detached_at_${tag}_/release/bin
-      break
-    elif [[ $version -eq "exit" ]]; then
-      exit
-    else
-      printf "\nChoose 1 or 2. Press ctrl + c or type 'exit' if you want to leave\n"
-    fi
-  done
+  if [[ $version -eq 1 ]]; then
+    printf "\n-> Building Master...\n"
+    dexec "git pull && git submodule update --init --force && make release-static"
+    copy monero-static-container:/home/monero/build/Linux/master/release/bin
+  elif [[ $version -eq 2 ]]; then
+    printf "\n-> Building release $tag\n"
+    dexec "git pull --all && git checkout ${tag} &> /dev/null && git submodule update --init --force && make release-static -j3"
+    copy monero-static-container:/home/monero/build/Linux/_HEAD_detached_at_${tag}_/release/bin
+  fi
 }
+
 
 # Introduction with Monero logo
 cat << "EOF"
@@ -102,13 +106,14 @@ if ! docker images | grep "monero-static" &> /dev/null; then
 
   printf "\n-> Building the Docker image...\n"
   get_tag
+  get_build
   docker build . -t monero-static || exit
   runcontainer
   build
-  printf "-> DONE"
 else
   printf "Welcome back! I'm about to create your Monero CLI release.\n"
   get_tag
+  get_build
   runcontainer
   build
 fi
